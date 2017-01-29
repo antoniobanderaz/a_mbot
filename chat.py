fdef9fde6dbe6f86ab50536ccd443691feb594aa
Alexander Davydov <mrgeneroth@gmail.com>
2017-01-30 02:39:31 +0300

    async generators

--- chat.py
+++ chat.py
@@ -6,44 +6,6 @@ import config
 SEND_TIMEOUT = 1 / config.RATE
 
 
-class AsyncIrcMessagesIterator:
-    def __init__(self, reader):
-        self._r = reader
-        self._resp = ''
-
-    def __aiter__(self):
-        return self
-
-    async def __anext__(self):
-        self._resp += (await self._r.read(1024)).decode('utf-8')
-        irc_messages = self._resp.split('\r\n')
-
-        if not self._resp.endswith('\r\n'):
-            self._resp = irc_messages.pop()
-        else:
-            self._resp = ''
-
-        return filter(None, irc_messages)
-
-
-class AsyncFlatIterator:
-    def __init__(self, aiter):
-        self._aiter = aiter
-        self._cur_iter = None
-
-    def __aiter__(self):
-        return self
-
-    async def __anext__(self):
-        while True:
-            if self._cur_iter is None:
-                self._cur_iter = iter(await self._aiter.__anext__())
-            try:
-                return next(self._cur_iter)
-            except StopIteration:
-                self._cur_iter = None
-
-
 class ChatSocket:
     def __init__(self, reader, writer, loop=None):
         self._r = reader
@@ -71,5 +33,16 @@ class ChatSocket:
 
         self._last_send_time = curr_send_time
 
-    def get_irc_messages(self):
-        return AsyncFlatIterator(AsyncIrcMessagesIterator(self._r))
+    async def get_irc_messages(self):
+        response = ''
+        while True:
+            response += (await self._r.read(1024)).decode('utf-8')
+            irc_messages = response.split('\r\n')
+
+            if not response.endswith('\r\n'):
+                response = irc_messages.pop()
+            else:
+                response = ''
+
+            for i in filter(None, irc_messages):
+                yield i
